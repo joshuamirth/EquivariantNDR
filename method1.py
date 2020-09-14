99168805a6f5088497a0f17e9b7bac5f83501653
Joshua Mirth <jmirth@mail.colostate.edu>
2020-09-14 10:33:43 -0400

    Basic script to generate test data.

--- method1.py
+++ method1.py
@@ -1,37 +1,84 @@
 # Hacked together script to do dim reduction on RPn
 import matlab.engine    # for LRCM MIN.
 import numpy as np
-# import DREiMac
+import scipy.io as io
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import floyd_warshall
+# This will only work in the folder containing dreimac.
+# from dreimac.projectivecoords import ppca
 
-# TODO: figure out importing of more general data.
-# Make some random data. Ultimately replace with something that imports
-# interesting data for testing.
-X = np.random.rand(3,20)
-data = X/np.linalg.norm(X,axis=0)
-# =============================================================================
-M = np.abs(data.transpose()@data)
+def circle_RPn(n=4,num_points=100,kinks=2)
+    """Construct points on a "kinked" circle in RP^n.
+
+    """
+
+    theta_size = np.floor(num_points/kinks)
+    if theta_size*kinks != num_points:
+        print('Warning: data will have ' + str(theta_size*kinks) +
+            ' points, not' + str(num_points))
+    theta = np.linspace(0,np.pi/2,num_points/kinks)
+    X = zeros((num_points,n+1))
+    for i in range(0:kinks):
+        X[i:theta_size+i,i:i+1] = [np.cos(theta),np.sin(theta)]
+    return X
+    
+
+def initial_guess(data,dim):
+    """Compute an initial guess for lrcm_min using projective pca."""
+    # Get projective coordinates in reduced dimension.
+    proj = ProjectiveCoords(
+        data,
+        n_landmarks = 100
+    )
+    res = proj.get_coordinates(proj_dim = dim, perc=0.9)
+    Y = res['X']
+#   Y = np.nan_to_num(Y)    # Apparently possible to have some values slightly out of range.
+    # Get an appropriate cholesky matrix to start lrcm_min.
+    YY = Y@Y.transpose()
+    YYd = YY[0:dim,0:dim]
+    print(np.linalg.matrix_rank(YYd))
+    R = np.linalg.cholesky(YYd) # Only compute cholesky of the upper corner.
+    Q = np.linalg.solve(YYd,Rd)
+    Y = Y@Q
+    return Y
+
+def sphere_toy(num_points,dim,embedding_dim):
+    """Get some points on the sphere of dimension dim and map up to
+    sphere of dimension embedding_dim."""
+    X = np.random.rand(num_points,dim)
+    S = (X.transpose()/np.linalg.norm(X,axis=1)).transpose()
+    S_high = np.zeros((num_points,embedding_dim))
+    S_high[:,0:dim] = S     # About as trivial an embedding as possible.
+    return S_high
+
+# dim = 8
+num_points = 10
+goal_dim = 2
+# X = get_line_patches(dim,10,10,0.25) # sample 100 points from line patches data.
+X = sphere_toy(num_points,goal_dim,goal_dim)
+Y0 = sphere_toy(num_points,goal_dim,goal_dim)
+#Y0 = initial_guess(X,2) # Get an initial guess in RP^2.
+#proj = ProjectiveCoords(X,n_landmarks=100)
+#res = proj.get_coordinates(proj_dim = dim**2-1, perc=0.9) # Coordinates in RP^n w/o reduction.
+#X = res['X']
+M = np.abs(X@X.transpose())
 # Due to rounding errors, M may contain values 1+epsilon. Remove these.
 bad_vals = M > 0.999999
 M[bad_vals] = 0.999999
 D = np.arccos(M)    # Initial distance matrix
 # Select neighborhoods using a radius (alt use k-NN)
-epsilon = 0.5
+epsilon = 1.0
 A = D<epsilon
 G = csr_matrix(D*A)                         # Matrix representation of graph
 Dg = floyd_warshall(G,directed=False)       # Path-length distance matrix
-# Implement a check here that the graph is actually connected (otherwise there 
-# are infinite values in the distance matrix).
+# Check that the graph is actually connected (otherwise there are infinite
+# values in the distance matrix).
 if np.isinf(np.max(Dg)):
     print("""Try a bigger value of epsilon - the distance matrix is not
         connected.""")
 Dhat = (np.max(D)/np.max(Dg))*Dg    # Normalize so distances are reasonable.
 W_inv = (1 - np.cos(Dhat)**2)       # Pointwise inverse of weight matrix.
-W = (W_inv+np.eye(20))**-1 - np.eye(20) # Put zeros on the diagonal w/o dividing by zero.
-
-Y0 = # figure out how to get a lower-triangular matrix for initial conditions.
-# Rank of this matrix determines rank of output.
+W = (W_inv+np.eye(num_points))**-1 - np.eye(num_points) # Put zeros on the diagonal w/o dividing by zero.
 
 S = np.sign(Y0@Y0.transpose())
 C = S*np.cos(Dhat)  # Cost matrix, in terminology of G&P.
@@ -41,10 +88,13 @@ C = S*np.cos(Dhat)  # Cost matrix, in terminology of G&P.
 
 # Switch over to matlab to call lrcm_min.
 print('Starting MATLAB ====================================================')
-scipy.io.savemat('ml_tmp.mat', dict(C=C,W=W,Y0=Y0))
+io.savemat('ml_tmp.mat', dict(C=C,W=W,Y0=Y0))
 eng = matlab.engine.start_matlab()
 t = eng.lrcm_wrapper()
-workspace = scipy.io.loadmat('py_tmp.mat')
+workspace = io.loadmat('py_tmp.mat')
 out_matrix = workspace['optimal_matrix']
 print('MATLAB complete ====================================================')
 
+C = out_matrix@out_matrix.transpose()
+print(C)
+print(M)
---
+++ testing_utils.py
@@ -0,0 +1,50 @@
+# Hacked together script to do dim reduction on RPn
+# import matlab.engine    # for LRCM MIN.
+import numpy as np
+# import scipy.io as io
+# from scipy.sparse import csr_matrix
+# from scipy.sparse.csgraph import floyd_warshall
+
+# This will only work in the folder containing dreimac:
+from projectivecoords import ppca
+
+
+def circle_RPn(dimn=4,segment_points=100,num_segments=2):
+    """Construct points on a "kinked" circle in RP^d.
+
+    Constructs a curve of evenly-spaced points along the great circle
+    from e_i to e_{i+1} in R^{d+1}, starting at e_0 and finishing at
+    e_i with i = num_segments, then returns to -e_0.
+
+    Parameters
+    ----------
+    dimn : int, optional
+        Dimension of RP^d to work on (ambient euclidean space is dimn+1).
+    segment_points : int, optional
+        Number of points along each segment of curve.
+    num_segments : int, optional
+        Number of turns to make before returning to start point.
+
+    Returns
+    -------
+    n : int
+        Number of points in the resulting curve, which is
+        segment_curve*(num_segments+1).
+    X : ndarray
+        Array of coordinate values in R^{d+1}.
+    """
+    
+    if dimn < num_segments:
+        raise ValueError('Dimension ' + str(dimn) + ' does not have enough ' +
+            'dimensions for ' + str(num_segments) + ' segments.')
+    num_points = segment_points*(num_segments+1)
+    theta = np.linspace(0,np.pi/2,segment_points,endpoint=False)
+    X = np.zeros((num_points,dimn+1))
+    segment_curve = np.array([np.cos(theta),np.sin(theta)]).transpose()
+    for i in range(0,num_segments):
+        X[i*segment_points:(i+1)*segment_points,i:i+2] = segment_curve
+    X[num_segments*segment_points:num_points,0] = -np.sin(theta)
+    X[num_segments*segment_points:num_points,num_segments] = np.cos(theta)
+    return {'n': num_points, 'X': X}
+
+def
