8d60c9205d40bdddbb61a16cff9a9a8d674508e8
Joshua Mirth <jmirth@mail.colostate.edu>
2020-09-17 15:19:58 -0400

    Updated notebooks.

--- test_nb.py
+++ test_nb.py
@@ -1,7 +1,19 @@
-# Embed a circle in RP^5 with two "kinks".
+# Embed a circle in RP^4 with five "kinks".
 import testing_utils
 import ppca
-X = testing_utils.circleRPn()
+X,n = testing_utils.circleRPn()
+
+# Do a dimensionality reduction from X to RP^2 using PPCA.
+V = ppca.ppca(X, 2)
+P = V['X']
+testing_utils.plot_RP2(P)
+
+# Do the dimensionality reduction from X to RP^2 using PMDS.
+M = testing_utils.pmds(X,2)
+testing_utils.plot_RP2(M)
+
+# Now do the same with RP^3 and four "kinks".
+X,n = testing_utils.circleRPn(dimn=3,num_segments=3)
 
 # Do a dimensionality reduction from X to RP^2 using PPCA.
 V = ppca.ppca(X, 2)
