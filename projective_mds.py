489efd9df9d47e7bfd5fb410b1f245a691668c02
Joshua Mirth <jmirth@mail.colostate.edu>
2020-09-24 14:48:22 -0400

    Updated to allow using pymanopt as solver.

--- projective_mds.py
+++ projective_mds.py
@@ -6,7 +6,13 @@ import scipy.io as io
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import floyd_warshall
 import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
+# Setup for pymanopt.
+import pymanopt
+from pymanopt.manifolds import Oblique 
+from pymanopt.solvers import TrustRegions
+from pymanopt.solvers import ConjugateGradient
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # dreimac does not install properly on my system.
 try:
@@ -25,7 +31,6 @@ def circleDM(n=50,diam=1.57):
     D = np.array([np.roll(theta,i) for i in range(0,2*n)])
     return D
     
-
 def circleRPn(
     dimn=4,
     segment_points=50,
@@ -75,7 +80,7 @@ def circleRPn(
     num_points = segment_points*(num_segments+1)
     theta = np.linspace(0,np.pi/2,segment_points,endpoint=False)
     X = np.zeros((num_points,dimn+1))
-    segment_curve = np.array([np.cos(theta),np.sin(theta)]).transpose()
+    segment_curve = np.array([np.cos(theta),np.sin(theta)]).T
     for i in range(0,num_segments):
         X[i*segment_points:(i+1)*segment_points,i:i+2] = segment_curve
     X[num_segments*segment_points:num_points,0] = -np.sin(theta)
@@ -84,8 +89,8 @@ def circleRPn(
         X = np.random.permutation(X)
     if noise:
         N = v*np.random.rand(dimn+1,num_points)
-        Xt = (X.transpose() + N)/LA.norm(X.transpose()+N,axis=0)
-        X = Xt.transpose()
+        Xt = (X.T + N)/LA.norm(X.T+N,axis=0)
+        X = Xt.T
     return X, num_points
 
 def initial_guess(data,dim):
@@ -117,7 +122,7 @@ def initial_guess(data,dim):
     # Testing out a completely random initial condition.
 #   X = 2*np.random.random_sample((dim+1,data.shape[0]))-1
 #   X = X/LA.norm(X,axis=0)
-#   X = X.transpose()
+#   X = X.T
     X = cholesky_rep(X)
     return X
 
@@ -127,7 +132,7 @@ def initial_guess_DM(D,dim):
     U,S,V = LA.svd(D)
     Uu = U[:,0:dim+1]
     nrm = LA.norm(Uu,axis=1)
-    Uu = (Uu.transpose()/nrm).transpose()
+    Uu = (Uu.T/nrm).T
     sgn = np.sign(np.random.rand(Uu.shape[0]-.5))
     sgn = sgn.reshape(Uu.shape[0],1)
     Uu = Uu*sgn
@@ -164,7 +169,7 @@ def cholesky_rep(X):
         raise LA.LinAlgError("""Input data not generic. If computing
             using circleRPn, check that dimn==num_segments.""")
     # Get an appropriate cholesky matrix to start lrcm_min.
-    L = LA.cholesky(Xd@Xd.transpose())
+    L = LA.cholesky(Xd@Xd.T)
     Q = LA.solve(Xd,L)
     C = np.tril(X@Q)    # The matrix is lower-triangular, but apply tril
     return C            # to handle floating-point errors.
@@ -205,7 +210,7 @@ def graph_distance_matrix(data,epsilon=0.4,k=-1):
 
     """
 
-    M = np.abs(data@data.transpose())
+    M = np.abs(data@data.T)
     # Due to rounding errors, M may contain values 1+epsilon. Remove these.
     bad_vals = M >= 1.0
     M[bad_vals] = 1.0
@@ -214,19 +219,20 @@ def graph_distance_matrix(data,epsilon=0.4,k=-1):
     if k > 0:
         D_sort = np.argsort(np.argsort(D))
         A = D_sort <= k
+        A = (A + A.T)/2
     # Use epsilon neighborhoods.
     else:
         A = D<epsilon
     G = csr_matrix(D*A)                   # Matrix representation of graph
     Dg = floyd_warshall(G,directed=False)     # Path-length distance matrix
     if np.isinf(np.max(Dg)):
-        raise ValueError("""The distance contains infinite values, indicating
-            that the graph is not connected. Try a larger value of epsilon or
-            k.""")
+        raise ValueError("""The distance matrix contains infinite values,
+            indicating that the graph is not connected. Try a larger value of
+            epsilon or k.""")
     Dhat = (np.max(D)/np.max(Dg))*Dg    # Normalize distances.
     return Dhat
 
-def pmds(Y,D,max_iter=20,verbose=True):
+def pmds(Y,D,max_iter=20,verbose=True,solver='pymanopt'):
     """Projective multi-dimensional scaling algorithm.
 
     Detailed description in career grant, pages 6-7 (method 1).
@@ -241,9 +247,12 @@ def pmds(Y,D,max_iter=20,verbose=True):
     max_iter : int, optional
         Number of times to iterate the loop. Will eventually be updated
         to a better convergence criterion. Default is 20.
-    verbose: bool, optional
+    verbose : bool, optional
         If true, print output relating to convergence conditions at each
         iteration.
+    solver : string, optional
+        Choice of algorithm for low-rank correlation matrix reduction.
+        Options are "pymanopt" or "matlab", default is "pymanopt".
 
     Returns
     -------
@@ -252,26 +261,37 @@ def pmds(Y,D,max_iter=20,verbose=True):
 
     # Put zeros on the diagonal of W w/o dividing by zero.
     num_points = Y.shape[0]
+    rank = LA.matrix_rank(Y)
+    if verbose:
+        print('Finding projection onto RP^%i.' %(rank-1))
     W_inv = (1 - np.cos(D)**2)     
     W = np.sqrt((W_inv+np.eye(num_points))**-1 - np.eye(num_points))
-    Y_prev = Y
-    S = np.sign(Y@Y.transpose())
+    S = np.sign(Y@Y.T)
     C = S*np.cos(D)
-    cost = []
+    cost_list = []
+    if solver == 'pymanopt':
+        manifold = Oblique(rank,num_points) # Short, wide matrices.
+        solver = ConjugateGradient()
+        Y_prev = Y.T
     for i in range(0,max_iter):
-        # Actual algorithmic loop:
-        workspace = lrcm_wrapper(C,W,Y)
-        # Updated information:
-        Y_new = workspace['optimal_matrix']
-        S_new = np.sign(Y_new@Y_new.transpose())
-        Fopt = workspace['Fopt']
-        cost.append(Fopt)
+        if solver == 'matlab':
+            # Actual algorithmic loop:
+            workspace = lrcm_wrapper(C,W,Y)
+            Y_new = workspace['optimal_matrix']
+        elif solver == 'pymanopt':
+            # Actual algorithmic loop:
+            cost, egrad, ehess = setup_cost(C,W)
+            problem = pymanopt.Problem(manifold, cost, egrad=egrad)
+            Y_new = solver.solve(problem,x=Y_prev)
+        S_new = np.sign(Y_new@Y_new.T)
         C_new = S_new*np.cos(D)
-        cost_newS = 0.5*LA.norm(W*(Y_new@Y_new.transpose()) - W*C_new)**2 
+        cost_oldS = 0.5*LA.norm(W*(Y_new@Y_new.T) - W*C)**2 
+        cost_list.append(cost_oldS)
+        cost_newS = 0.5*LA.norm(W*(Y_new@Y_new.T) - W*C_new)**2 
         S_diff = ((LA.norm(S_new - S))**2)/4
         percent_S_diff = 100*S_diff/S_new.size
         if i > 0:
-            percent_cost_diff = 100*(cost[i-1] - cost[i])/cost[i-1]
+            percent_cost_diff = 100*(cost_list[i-1] - cost_list[i])/cost_list[i-1]
         else:
             percent_cost_diff = 100
         # (This is not actually necessary - not doing so gives us "mean
@@ -280,7 +300,7 @@ def pmds(Y,D,max_iter=20,verbose=True):
         # Y,s,vh = LA.svd(out_matrix,full_matrices=False)
         if verbose:
             print('Through %i iterations:' %(i+1))
-            print('\tComputed cost: %2.2f' %cost[i])
+            print('\tComputed cost: %2.2f' %cost_list[i])
             print('\tPercent cost difference: % 2.2f' %percent_cost_diff)
             print('\tPercent Difference in S: % 2.2f' %percent_S_diff)
             print('\tComputed cost with new S: %2.2f' %cost_newS)
@@ -294,11 +314,11 @@ def pmds(Y,D,max_iter=20,verbose=True):
             print('No significant cost improvement. Stopping iterations.')
             break
         # Update variables:
-        Y_prev = Y
+        Y_prev = Y.T
         Y = Y_new
         C = C_new
         S = S_new
-    return Y, cost, Y_prev
+    return Y, cost_list, Y_prev
 
 def lrcm_wrapper(C,W,Y0):
 #   print('Starting MATLAB ==================================================')
@@ -322,3 +342,115 @@ def plot_RP2(X,pullback=True):
         Y = -X
         ax.scatter(Y[:,0],Y[:,1],Y[:,2])
     plt.show()
+
+#def fancy_pmds(Y,D,dim,max_iter=20,verbose=True):
+#   """Implement the same algorithm, but using pymanopt to do the
+#   optimization step.
+
+#   Pymanopt documentation is here: https://www.pymanopt.org/
+
+#   Parameters
+#   ----------
+#   D : ndarray
+#       Distance matrix.
+#   rank : int
+#       Rank of matrix to optimize onto. Rank d+1 means result on RP^d.
+
+#   Returns
+#   -------
+#   Y : ndarray
+#       Optimal configuration.
+
+#   """
+
+#   # Setup the cost functions.
+#   W_inv = (1 - np.cos(D)**2)     
+#   W = np.sqrt((W_inv+np.eye(size))**-1 - np.eye(size))
+#   S = np.sign(Y@Y.T)
+#   C = S*np.cos(D)
+#   
+#   manifold = Oblique(rank,num_points) # Short, wide matrices.
+#   solver = ConjugateGradient()
+#   Y_prev = Y.T
+#   for i in range(0,max_iter):
+#       # Actual algorithmic loop:
+#       cost, egrad, ehess = setup_cost(C,W)
+#       problem = pymanopt.Problem(manifold, cost, egrad=egrad)
+#       Y_new = solver.solve(problem,x=Y_prev)
+#       # Updated information:
+#       Y_new = Y_new.T
+#       S_new = np.sign(Y_new@Y_new.T)
+#       C_new = S_new*np.cos(D)
+#       cost_oldS = 0.5*LA.norm(W*(Y_new@Y_new.T) - W*C)**2 
+#       cost_list.append(cost_oldS)
+#       cost_newS = 0.5*LA.norm(W*(Y_new@Y_new.T) - W*C_new)**2 
+#       S_diff = ((LA.norm(S_new - S))**2)/4
+#       percent_S_diff = 100*S_diff/S_new.size
+#       if i > 0:
+#           percent_cost_diff = 100*(cost_list[i-1] -
+#ost_list[i])/cost_list[i-1]
+#       else:
+#           percent_cost_diff = 100
+#       # (This is not actually necessary - not doing so gives us "mean
+#       # centered" data.)
+#       # Do an SVD to get the correlation matrix on the sphere.
+#       # Y,s,vh = LA.svd(out_matrix,full_matrices=False)
+#       if verbose:
+#           print('Through %i iterations:' %(i+1))
+#           print('\tComputed cost: %2.2f' %cost_list[i])
+#           print('\tPercent cost difference: % 2.2f' %percent_cost_diff)
+#           print('\tPercent Difference in S: % 2.2f' %percent_S_diff)
+#           print('\tComputed cost with new S: %2.2f' %cost_newS)
+#           print('\tDifference in cost matrix: %2.2f' %(LA.norm(C-C_new)))
+#           print('\tPercent of orthogonal elements: %2.2f' %ortho_ips)
+#           print('\tPercent of negligible cost terms: %2.2f' %neg_cost)
+#       if S_diff < 1:
+#           print('No change in S matrix. Stopping iterations.')
+#           break
+#       if percent_cost_diff < .0001:
+#           print('No significant cost improvement. Stopping iterations.')
+#           break
+#       # Update variables:
+#       Y_prev = Y.T
+#       Y = Y_new
+#       C = C_new
+#       S = S_new
+#   return Y, cost_list
+
+def setup_cost(C,W):
+    """Create the cost functions.
+
+    Parameters
+    ----------
+    C : ndarray
+        Cost matrix
+    S : ndarray
+        Sign matrix
+
+    Returns
+    -------
+    cost : function
+        Cost function for problem.
+    grad : function
+        Gradient of cost function.
+    hess : function
+        Hessian of cost function.
+
+    """
+    
+    def F(Y):
+        """Weighted Frobenius norm cost function."""
+        return 0.5*LA.norm(W*(C-Y.T@Y), 'fro')**2
+
+    def dF(Y):
+        """Derivative of weighted Frobenius norm cost."""
+        Psi = Y.T@Y-C
+        return 2*Y@(W*Psi)
+
+    def ddF(Y,H):
+        """Second derivative (Hessian) of weighted Frobenius norm cost."""
+        Psi = Y.T@Y-C
+        return 2*((W*Psi)@H + (W*(Y@H.T + H@Y.T))@Y)
+    
+    return F, dF, ddF
+
