7ba16b578f9a7a1e2462107c333ebb532ce59ee6
Joshua Mirth <jmirth@mail.colostate.edu>
2020-11-11 10:31:16 -0500

    Started grassmannian version.

---
+++ grassmann_mds.py
@@ -0,0 +1,12 @@
+import autograd.numpy as np
+import autograd.numpy.linalg as LA
+import pymanopt
+from pymanopt.manifolds import Oblique  # Change this one.
+from pymanopt.solvers import *
+
+def gmds():
+
+def grassmannian_cost():
+
+
+# Represent points on the Grassmannian as either 
