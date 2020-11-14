57ec858891f521897a2a85d323f18e54d85c654e
Joshua Mirth <19397711+joshuamirth@users.noreply.github.com>
2020-11-14 09:22:36 -0500

    Finished lens coordinate function.

--- pipeline.py
+++ pipeline.py
@@ -59,7 +59,7 @@ def prominent_cocycle(D,q=2,epsilon=1e-3):
             'specified.' %q)
     birth = diagram[index,0]
     death = diagram[index,1]
-    if death < 2*birth+epsilon:
+    if death < 2*birth + epsilon:
         valid_class = False
     else:
         valid_class = True
@@ -90,47 +90,6 @@ def partition_unity_jrm(D,radius,landmarks,conical=False):
         to one, and the `j`-th column gives the value of the partition
         function subordinate to the open ball centered at landmark `j`.
 
-    """
-
-    U = D[landmarks,:]
-    S = U<epsilon
-    if conical:
-        S = S*U
-    if not np.all(np.sum(S,axis=0)):
-        raise DivisionByZeroError('Open sets do not cover data when epsilon '\
-            '= %d.',%epsilon)
-    else:
-        S = S/np.sum(S,axis=0)
-        return S
-
-def lens_coordinates_jrm(
-    distance_matrix,
-    partition_function,
-    cocycle,
-    p,
-    N_landmarks
-):
-    """Coordinates of data matrix in lens space.
-    
-    Parameters
-    ----------
-    dist_matrix : ndarray (n*n)
-        Distance matrix of dataset.
-    partition_function : ndarray (l*n)
-        Function describing a partition of unity on the open cover given
-        by a landmark subset with `l` elements.
-    cocycle : ndarray (l*3)
-        Cocycle representing choice of persistent H_1 class. The first
-        two columns give the simplices as pairs [i,j]. The third column
-        is the value in :math:`\mathbb{Z}_p` corresponding to each
-        simplex. See documentation for ripser [1]_.
-
-    Returns
-    -------
-    X : ndarray (n*n)
-        Coordinates of data in lens space :math:`L_p^n`. Each column is
-        a complex vector with unit norm (a point on :math:`S^{2n-1}`).
-
     Examples
     --------
     >>> D = numpy.array([[0,1,2,1,0.7],
@@ -138,11 +97,71 @@ def lens_coordinates_jrm(
             [2,1,0,1,1.7],
             [1,2,1,0,0.7],
             [0.7,1.7,1.7,0.7,0]])
+    >>> radius = 1.05
     >>> landmarks = [0,1,2,3]
-    >>> partition_function = LPCA.partition_unity_jrm(D,1.0,landmarks)
-    >>> cocycle = numpy.array([[1,0,1])
-    >>> lens_coordinates_jrm(D,partition_function,cocycle,2)
+    >>> partition_unity(D,radius,landmarks)
+    array([[0.33333333, 0.33333333, 0.        , 0.33333333, 0.5       ],
+           [0.33333333, 0.33333333, 0.33333333, 0.        , 0.        ],
+           [0.        , 0.33333333, 0.33333333, 0.33333333, 0.        ],
+           [0.33333333, 0.        , 0.33333333, 0.33333333, 0.5       ]])
+
+    """
+
+    U = D[landmarks,:]
+    S = U < radius
+    if conical:
+        S = S*U
+    if not np.all(np.sum(S,axis=0)):
+        raise DivisionByZeroError('Open sets do not cover data when epsilon '\
+            '= %d.' %epsilon)
+    else:
+        S = S/np.sum(S,axis=0)
+        return S
+
+def lens_coordinates_jrm(
+    partition_function,
+    cocycle,
+    p,
+):
+    """Coordinates of data matrix in lens space.
     
+    Parameters
+    ----------
+    partition_function : ndarray (d*N)
+        Function describing a partition of unity on the open cover given
+        by a landmark subset. Each of the `d` rows corresponds to the
+        function on the open ball around a landmark, where the `i`-th
+        entry is the value of that function on the `i`-th data point.
+    cocycle : ndarray (l*3)
+        Cocycle representing choice of persistent H_1 class. The first
+        two columns give the simplices as pairs [i,j]. The third column
+        is the value in :math:`\mathbb{Z}_p` corresponding to each
+        simplex. See documentation for ripser [1]_.
+    p : int (prime power)
+        Choice of quotient for lens space coordinates. Must match the
+        coefficient field in which the cocycle takes values. (So if
+        homology was computed with :math:`\mathbb{Z}_3` coefficients,
+        need to set `p = 3`.)
+        
+    Returns
+    -------
+    X : ndarray (n*N)
+        Coordinates of data in lens space :math:`L_p^d`. Each column is
+        a complex vector with unit norm (a point on :math:`S^{2n-1}`).
+
+    Examples
+    --------
+    >>> partition_function = numpy.array([[0.3333, 0.3333, 0., 0.3333, 0.5],
+            [0.3333, 0.3333, 0.3333, 0., 0.],
+            [0., 0.3333, 0.3333, 0.3333, 0.],
+            [0.3333, 0., 0.3333, 0.3333, 0.5]])
+    >>> cocycle = numpy.array([[1,0,1])
+    >>> p = 2
+    >>> lens_coordinates_jrm(D,partition_function,cocycle,p)
+    array([[ 0.57735027+0.j, 0.57735027+0.j, -0.+0.j, 0.57735027+0.j, 0.70710678+0.j],
+           [ 0.57735027+0.j, 0.57735027+0.j, 0.57735027+0.j, 0.+0.j,  0.+0.j],
+           [ 0.+0.j, 0.57735027+0.j, 0.57735027+0.j, 0.57735027+0.j, 0.+0.j],
+           [ 0.57735027+0.j, 0.+0.j, 0.57735027+0.j, 0.57735027+0.j, 0.70710678+0.j]])   
 
     References
     ----------
@@ -150,31 +169,22 @@ def lens_coordinates_jrm(
     
     """
 
-    simplices = cocycle[:,0:2]
-    cocycle_values = cocycle[:,2]
-    # Pretty sure that simplices are indexed exactly as the distance
-    # matrix is, so that [0,1] is an edge between the (points
-    # representing the) first and second rows of the distance matrix,
-    # for example. TODO: double check this assumption.
-    # For each landmark point (i.e. each cover set):
-        # Construct the function for that index.
-        # For each row in distance matrix:
-            #   If row is covered by landmark:
-                # Apply f_landmark to row.
-                # Mark row as done.
-    n = D.shape[0]
-    used_rows = np.zeros(n)
-    zeta_p = np.exp(2j*np.pi/p)*np.ones(n)
-    X = np.zeros(n,n)
-    for i in range(N_landmarks):
-        tmp_eta = np.zeros(N_landmarks)
-        idx = np.where(simplices[:,0]==i)
-        tmp_eta[idx] = cocycle_values[idx]
-        zeta_i = zeta_p**tmp_eta
-        for k in range(n):
-            if partition_function[k,i] != 0 and not used_rows[k]:
-                used_rows[k] = 1
-                X[:,k] = np.sqrt(partition_function[i,:])*zeta_i
+    d = partition_function.shape[0] # number of landmarks.
+    N = partition_function.shape[1] # number of data points.
+    used_columns = np.zeros(N)
+    zeta = np.exp(2j*np.pi/p)
+    X = np.zeros((d,N),dtype=complex) # set to complex to avoid unsafe cast.
+    for i in range(d):
+        tmp_eta = np.zeros(d)
+        idx = np.where(cocycle[:,0]==i)
+        # TODO: double check that cocycles from ripser come sorted in the
+        # second column, otherwise need to implement that here.
+        tmp_eta[idx] = cocycle[:,2][idx]
+        zeta_i = zeta**tmp_eta
+        for j in range(N):
+            if partition_function[i,j] != 0 and not used_columns[j]:
+                used_columns[j] = 1
+                X[:,j] = np.sqrt(partition_function[:,j])*zeta_i
     return X
 
 
