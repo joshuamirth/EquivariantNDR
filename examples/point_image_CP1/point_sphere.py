df4a52e4df41fc326522dc253f16e3f641037f82
Joshua Mirth <19397711+joshuamirth@users.noreply.github.com>
2021-02-02 11:20:27 -0500

    Added folder for point images code.

---
+++ examples/point_image_CP1/point_sphere.py
@@ -0,0 +1,10 @@
+N = 24     # Create N*N images
+amp = 1
+sd = .5
+
+# Do some kind of loop over the N*N grid
+# Place a Gaußian at the point selected
+
+# This example is actually tricky because we will need to apply EM
+# coordinates to make this into a proper sphere, and not just something
+# with spherical homology.
