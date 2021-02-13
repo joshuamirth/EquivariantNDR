36f063c3b413644f7e6dccb00afa020e852a8963
Joshua Mirth <19397711+joshuamirth@users.noreply.github.com>
2021-02-13 13:55:02 -0500

    Improved chordal metric method for RP^n.

--- chordal_metric.py
+++ chordal_metric.py
@@ -13,16 +13,22 @@ def sine_distance_matrix(X):
     D = np.sqrt(1 - (X.T@X)**2)
     return D
 
-def rp_mds(D, dim, X=None):
+def rp_mds(D, dim=3, X=None):
     """MDS via gradient descent with sine metric.
 
     Parameters
     ----------
     D : ndarray (n, n)
         Goal distance matrix.
-    X : ndarray (n, dim), optional
-        Initial value for gradient descent. `n` points in dimension `dim`.
+    dim : int, optional
+        Goal dimension (of ambient Euclidean space). Default is `dim = 3`.
+    X : ndarray (dim, n), optional
+        Initial value for gradient descent. `n` points in dimension `dim`. If
+        both a dimension and an initial condition are specified, the initial
+        condition overrides the dimension.
+
     """
+
     n = D.shape[0]
     max_d = np.max(D)
     if max_d > 1:
@@ -32,10 +38,13 @@ def rp_mds(D, dim, X=None):
     solver = pymanopt.solvers.ConjugateGradient()
     cost = setup_cost(D)
     problem = pymanopt.Problem(manifold=manifold, cost=cost)
-    if X:
-        X_out = solver.solve(problem, x=X)
-    else:
+    if X is None:
         X_out = solver.solve(problem)
+    else:
+        if X.shape[0] != dim:
+            print('WARNING: initial condition does not match specified goal '\
+                'dimension. Finding optimum in dimension %d' %X.shape[0])
+        X_out = solver.solve(problem, x=X)
     return X_out
 
 def setup_cost(D):
